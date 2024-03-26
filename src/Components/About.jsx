8a19e1348b97f1304fd2cc76931b3ecb6c85159b
Marcella Harris <marcellalharris@yahoo.com>
2024-03-26 03:41:02 +0000

    Remove random period

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a Data Analyst student studying at Western Governors University. I am most happy working on my personal projects and going to school.";
+  "I am a Data Analyst student studying at Western Governors University. I am most happy working on my personal projects and going to school.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
